e45a08d7b2dce247e98bec61f62f1b753b981d0d
SiamIslam-Admin <siamislam654321@gmail.com>
2026-03-08 09:56:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ async def main():
         cookies="stel_ssid=73fe99ee138ed81a27_36735179866457374; stel_token=7a6e0889befdc57613c4f245127815287a6e08937a6e0061de7d3a52a28883e689a88",
         hash_value="c3d0ea624e192dd63c",
         wallet_mnemonic="differ color hold primary ensure illegal clap orange borrow lock essence verify pipe exchange auto venue flock jaguar black submit garbage injury city jealous",
-        wallet_api_key="462217391156217a1be5819e36e69d389016b87c97242cd481ebe73e39731434"
+        wallet_api_key="462217391156217a1be5819e36e69d389016b87c97242cd481ebe73e39731434",
+        wallet_version="V4R2"   # try V4R2 first
+        # wallet_version="W5"    # if your wallet is V5, test this instead
     )
 
     try:
